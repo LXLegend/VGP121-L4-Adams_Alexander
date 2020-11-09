5038ed8915a6ad26e37a8acfe675e41d9b7dd9a5
LXLegend <69595735+LXLegend@users.noreply.github.com>
2020-11-09 10:02:14 -0500

    Camera Movement script added

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = .125f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, 0) + offset;
+        /*
+        Vector2 smoothedPosition = Vector2.Lerp(transform.right, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        */
+        transform.position = desiredPosition;
+    }
+}
